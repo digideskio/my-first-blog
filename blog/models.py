4b74fc16b5a9b20cf355e77dd65b80e44a885815
elhombretecla <delacruzgarciajuan@gmail.com>
2016-02-27 12:28:24 +0100

    Modified templates to display posts from database

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,5 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
 from django.utils import timezone
-
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
